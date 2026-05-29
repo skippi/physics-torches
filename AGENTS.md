a9d109e18a07dbe7e017e8ce3984a37b4ed023ef
skippi <jbtcao@gmail.com>
2026-05-29 11:45:06 -0500

    feat: add temp torch rigidbody

---
+++ AGENTS.md
@@ -0,0 +1,5 @@
+# AGENTS.md
+
+## Decompiling
+
+- Decompile code into build/llm-cache. There's no need to clean up any code from this directory when you decompile code. You can look up here to see if you already decompiled something.
--- src/client/java/com/binhjcao/physicstorches/client/EntityPhysicsTorchRenderer.java
+++ src/client/java/com/binhjcao/physicstorches/client/EntityPhysicsTorchRenderer.java
@@ -33,6 +33,9 @@ public class EntityPhysicsTorchRenderer
     state.movingBlockRenderState.randomSeedPos = blockPos;
     state.movingBlockRenderState.blockPos = blockPos;
     state.movingBlockRenderState.blockState = entity.getBlockState();
+    state.tilt = entity.getXRot(partialTick);
+    state.tiltYaw = entity.getYRot(partialTick);
+    state.roll = entity.getRoll(partialTick);
 
     if (entity.level() instanceof ClientLevel clientLevel) {
       state.movingBlockRenderState.biome = clientLevel.getBiome(blockPos);
@@ -52,8 +55,14 @@ public class EntityPhysicsTorchRenderer
       return;
     }
 
+    float pivotY = EntityPhysicsTorch.pivotYOffset(state.tilt);
+
     poseStack.pushPose();
     poseStack.translate(-0.5D, 0.0D, -0.5D);
+    poseStack.translate(0.5D, pivotY, 0.5D);
+    poseStack.mulPose(
+        EntityPhysicsTorch.torchRotation(state.tilt, state.tiltYaw, state.roll));
+    poseStack.translate(-0.5D, -pivotY, -0.5D);
     queue.submitMovingBlock(poseStack, state.movingBlockRenderState);
     poseStack.popPose();
     super.submit(state, poseStack, queue, cameraState);
@@ -61,5 +70,8 @@ public class EntityPhysicsTorchRenderer
 
   public static final class TorchRenderState extends EntityRenderState {
     public final MovingBlockRenderState movingBlockRenderState = new MovingBlockRenderState();
+    public float tilt;
+    public float tiltYaw;
+    public float roll;
   }
 }
